f7f4999673502a109c5daa2f4337ed2e9fc0abeb
takenoto <mendesrenan5@gmail.com>
2023-01-18 16:32:02 -0300

    updated params_list for grid_search loop

--- main/run_model.py
+++ main/run_model.py
@@ -14,6 +14,7 @@ from domain.reactions_ode_system_preparers.ode_preparer import ODEPreparer
 
 import tensorflow as tf
 
+
 def run_model(
     solver_params_list=None,
     eq_params=None,
@@ -25,10 +26,8 @@ def run_model(
     """
     f_out_value_calc --> f_out_value_calc(max_reactor_volume, f_in_v, volume)
     """
-    
     assert f_out_value_calc is not None, "f_out_value_calc is necessary"
 
-    print("Starting Batch Model")
 
     if solver_params_list is None:
 
@@ -41,61 +40,95 @@ def run_model(
             num_domain=600,
             num_boundary=10,
             num_test=1000,
-            adam_epochs=7500, 
+            adam_epochs=8500,
             adam_display_every=300,
             adam_lr=0.0001,
             layer_size=[1] + [36] * 4 + [4],
             activation="tanh",
             initializer="Glorot uniform",
+            # Só funciona com esse w volume 10+. Em 4 e 1 não funcionou, porque
+            # o volume acaba caindo muito
+            w_volume=10,
             # Faz com que não use o modo adimensional
-            non_dim_scaler=NonDimScaler()
+            non_dim_scaler=NonDimScaler(t=1),
         )
 
-        # V2
-        # solver_params = SolverParams(
-        #     num_domain=600,
-        #     num_boundary=10,
-        #     num_test=1000,
-        #     adam_epochs=5000,
-        #     adam_display_every=200,
-        #     adam_lr=0.0001,
-        #     layer_size=[1] + [32] * 4 + [4],
-        #     activation="tanh",
-        #     initializer="Glorot uniform",
-        #     # TODO testa com non dim scaler com tudo no 1. Tem algo estranho.
-        #     # Se prestar: como testar novos tempos????
-        #     # non_dim_scaler = NonDimScaler(),
-        #     non_dim_scaler=NonDimScaler(
-        #         X=eq_params.Xm,
-        #         P=eq_params.Pm,
-        #         S=eq_params.So,
-        #         V=process_params.max_reactor_volume,
-        #         t=0.03#process_params.t_final
-        #     )
-        # )
-
-        #V3
-        # solver_params = SolverParams(
-        #     num_domain=1400,
-        #     num_boundary=10,
-        #     num_test=1000,
-        #     adam_epochs=1500,
-        #     adam_display_every=300,
-        #     adam_lr=0.0001,
-        #     layer_size=[1] + [28] * 3 + [4],
-        #     activation="tanh",
-        #     initializer="Glorot uniform",
-        #     non_dim_scaler=NonDimScaler(
-        #         X=eq_params.Xm,
-        #         P=eq_params.Pm,
-        #         S=eq_params.So,
-        #         V=process_params.max_reactor_volume,
-        #         t=16
-        #     )
-        #     # O x é muito sensível e ele descarrilha todos os outros
-        # )
-
+        # Teste 1: Se for adimensinal, ainda tem essa dependência de peso pro volume?
+        # Curiosamente as concs tem de 0.9 e de 50 mas não pesa tanto
+        # e o volume, em 5, esculhamba tudo
+        # w_V =1 e V_scale = process_params.max_reactor_volume o volume ainda cai sei controle
+        # w_V =9 e V_scale = process_params.max_reactor_volume o volume ainda não tá bom
+        # Aparentemente quanto maior o scale_V mais descarrilha e demora a convergir
+        solver_params = SolverParams(
+            num_domain=600,
+            num_boundary=10,
+            num_test=1000,
+            adam_epochs=8500,
+            adam_display_every=300,
+            adam_lr=0.0001,
+            layer_size=[1] + [36] * 4 + [4],
+            activation="tanh",
+            initializer="Glorot uniform",
+            # Só funciona com esse w volume 10+. Em 4 e 1 não funcionou, porque
+            # o volume acaba caindo muito
+            w_volume=9,
+            # Faz com que não use o modo adimensional
+            non_dim_scaler=NonDimScaler(V=process_params.max_reactor_volume * 5, t=1),
+        )
+        
         solver_params_list = [solver_params]
+        
+        #---------------------------------------
+        #---------------------------------------
+        # NOVO
+        # Fração pela qual os valores serão divididos dentro do loop. Precisa ser >= 1 e inteiro
+        frac = 1 # 10
+        w_frac = 1 # fração a se testar os weights do retorno da rede
+        solver_params_list = [
+            SolverParams(
+                num_domain=600,
+                num_boundary=10,
+                num_test=1000,
+                adam_epochs=8500,
+                adam_display_every=3000,
+                adam_lr=0.0001,
+                layer_size=layer_size,
+                activation="tanh",
+                initializer="Glorot uniform",
+                w_X=X_weight,
+                w_P=P_weight,
+                w_S=S_weight,
+                w_volume=V_weight,
+                non_dim_scaler=NonDimScaler(
+                    X=eq_params.Xm*(X_scaler / frac),
+                    P=eq_params.Pm*(P_scaler / frac),
+                    S=eq_params.So*(S_scaler / frac),
+                    V=process_params.max_reactor_volume * (V_scaler / frac),
+                    t=process_params.t_final * (t_scaler / frac),
+                ),
+            )
+            for layer_size in [
+                # Muito espalhadas
+                [1] + [8] * 22 + [4],
+                [1] + [4] * 40 + [4],
+                # Muito concentradas
+                [1] + [140] * 2 + [4],
+                [1] + [320] * 1 + [4],
+                # Equilibradas
+                [1] + [22] * 3 + [4],                
+                [1] + [36] * 4 + [4],                
+                [1] + [80] * 5 + [4],                
+            ]
+            for X_scaler in range(1, frac+1)
+            for P_scaler in range(1, frac+1)
+            for S_scaler in range(1, frac+1)
+            for V_scaler in range(1, frac+1)
+            for t_scaler in range(1, frac+1)
+            for X_weight in range(1, w_frac+1)
+            for P_weight in range(1, w_frac+1)
+            for S_weight in range(1, w_frac+1)
+            for V_weight in range(1, w_frac+1)
+        ]
 
     # ---------------------------------------
 
@@ -104,8 +137,7 @@ def run_model(
         initial_state
         if initial_state
         else CSTRState(
-            volume=np.array([4]),  # L
-            # TODO teste Xo=0, tudo deve ficar constante
+            volume=np.array([4]),
             X=eq_params.Xo,
             P=eq_params.Po,
             S=eq_params.So,
@@ -113,7 +145,7 @@ def run_model(
     )
 
     if process_params is None:
-        # Se não for especificado, assume que entrada é 0
+        # If the inlet flow is not specified, it is assumed that it is 0 L/h
         inlet = ConcentrationFlow(
             volume=0,
             X=eq_params.Xo,
@@ -121,10 +153,13 @@ def run_model(
             S=eq_params.So,
         )
         process_params = ProcessParams(max_reactor_volume=5, inlet=inlet, t_final=16)
+    
+    # Se plot params for nulo cria um padrão
+    plot_params = plot_params if plot_params else PlotParams(force_y_lim=True)
 
-    plot_params = plot_params if plot_params else PlotParams(force_y_lim=False)
-
-    ode_system_preparer = ODEPreparer(solver_params, eq_params, process_params, f_out_value_calc)
+    ode_system_preparer = ODEPreparer(
+        solver_params, eq_params, process_params, f_out_value_calc
+    )
 
     # Define um caller para os parâmetros atuais (só necessita do solver depois)
     run_reactor_system_caller = RunReactorSystemCaller(
@@ -132,7 +167,7 @@ def run_model(
         eq_params=eq_params,
         process_params=process_params,
         initial_state=initial_state,
-        plot_params=plot_params,  # Se plot params for nulo cria um padrão
+        plot_params=plot_params,  
         f_out_value_calc=f_out_value_calc,
     )
 
