0ea5c3f32d8ee2055d2759bac71378c20d687512
takenoto <mendesrenan5@gmail.com>
2023-03-14 09:02:31 -0300

    deletei coisas antigas inúteis

--- main/main.py
+++ main/main.py
@@ -69,106 +69,6 @@ num_colors = [
     "#F39B6D",
     "#F0C987",
 ]
-
-
-def plot_compare_pinns_and_num(pinns, nums, eq_params, title=None):
-    # --------------------------------------
-    # PLOTTING
-    # --------------------------------------
-
-    # Plot simple color bar with ts, step, loss as x, y, z
-    ts, step, loss = get_ts_step_loss_as_xyz(pinns=pinns)
-    if False:
-        plot_simple_color_bar(
-            title="Time scaling factor, number of steps and error",
-            x=ts,
-            y=step,
-            z=loss,
-            x_label="t_s",
-            y_label="step",
-            z_label="loss",
-        )
-
-    # PLOTAR X
-    multiplot_xpsv(
-        title=title if title else "Concentrations over time for different methods",
-        y_label="g/L",
-        x_label="time (h)",
-        t=[n.t for n in nums] + [pinn.t for pinn in pinns],
-        X=[n.X for n in nums] + [pinn.X for pinn in pinns],
-        P=None,
-        S=None,
-        V=None,
-        y_lim=[0, eq_params.Xo * 10],
-        # Essa linha só é interessante quando o erro é baixo...
-        # err=[0 for n in nums] + [pinn.best_loss_test for pinn in pinns],
-        scaler=[n.non_dim_scaler for n in nums]
-        + [pinn.solver_params.non_dim_scaler for pinn in pinns],
-        suffix=[n.model_name for n in nums] + [pinn.model_name for pinn in pinns],
-        plot_args=[
-            XPSVPlotArg(
-                ls="-",
-                color=num_colors[i % len(num_colors)],
-                linewidth=7.0,
-                alpha=0.25,
-            )
-            for i in range(len(nums))
-        ]
-        + [
-            XPSVPlotArg(
-                ls="--",
-                color=pinn_colors[i % len(pinn_colors)],
-                linewidth=4,
-                alpha=1,
-            )
-            for i in range(len(pinns))
-        ],
-    )
-
-    # PLOTAR V
-    multiplot_xpsv(
-        title=title if title else "Reactor content volume over time",
-        y_label="L",
-        x_label="time (h)",
-        t=[n.t for n in nums] + [pinn.t for pinn in pinns],
-        X=None,
-        P=None,
-        S=None,
-        V=[n.V for n in nums] + [pinn.V for pinn in pinns],
-        y_lim=[0, eq_params.Xo * 10],
-        # Essa linha só é interessante quando o erro é baixo...
-        # err=[0 for n in nums] + [pinn.best_loss_test for pinn in pinns],
-        scaler=[n.non_dim_scaler for n in nums]
-        + [pinn.solver_params.non_dim_scaler for pinn in pinns],
-        suffix=[n.model_name for n in nums] + [pinn.model_name for pinn in pinns],
-        plot_args=[
-            XPSVPlotArg(
-                ls="-",
-                color=num_colors[i % len(num_colors)],
-                linewidth=7.0,
-                alpha=0.25,
-            )
-            for i in range(len(nums))
-        ]
-        + [
-            XPSVPlotArg(
-                ls="--",
-                color=pinn_colors[i % len(pinn_colors)],
-                linewidth=4,
-                alpha=1,
-            )
-            for i in range(len(pinns))
-        ],
-    )
-
-    if len(pinns) >= 1:
-        plot_3d_surface_ts_step_error(pinns=pinns)
-
-    if True:
-        plot_lines_error_compare(pinns=pinns, group_by="t_s")
-
-
-
     
 
 def main():
@@ -192,12 +92,7 @@ def main():
 
     run_weights = False
 
-    run_cstr = False
-
-    run_fed_batch = False
-
-    run_batch = False
-
+   
     altiok_models_to_run = [get_altiok2006_params().get(2)]  # roda só a fig2
 
     # Parâmetros de processo (será usado em todos)
@@ -285,7 +180,7 @@ def main():
 
         plot_comparer_multiple_grid(
             labels=['Loss (teste)', 'Loss (treino)'],
-            figsize=(7.2, 8.2),
+            figsize=(7.2*1.5, 8.2*1.5),
             gridspec_kw={"hspace": 0.35, "wspace": 0.14},
             yscale='log',
             sharey=True,
@@ -336,7 +231,7 @@ def main():
             plot_comparer_multiple_grid(
                 suptitle=pinn.model_name,
                 labels=['Euler', 'PINN'],
-                figsize=(6, 8),
+                figsize=(6*1.5, 8*1.5),
                 gridspec_kw={"hspace": 0.6, "wspace": 0.25},
                 yscale='linear',
                 sharey=False,
@@ -344,7 +239,7 @@ def main():
                 ncols=1,
                 items=items,
                 title_for_each=True,
-                supxlabel="time (h)",
+                supxlabel="tempo (h)",
                 supylabel="g/L",
             )
 
@@ -1020,158 +915,6 @@ def main():
         )
         pass
 
-    if run_cstr:
-        initial_state = CSTRState(
-            volume=np.array(1),  # antes era [1]
-            X=eq_params.Xo,
-            P=eq_params.Po,
-            S=eq_params.So,
-        )
-
-        process_params = ProcessParams(
-            max_reactor_volume=5,
-            inlet=ConcentrationFlow(
-                volume=0.07,
-                X=eq_params.Xo,
-                P=eq_params.Po,
-                S=eq_params.So,
-            ),
-            t_final=24,
-        )
-
-        def cstr_f_out_calc_numeric(max_reactor_volume, f_in_v, volume):
-            return f_in_v * (1 + (99 * volume / max_reactor_volume)) / 100
-            if volume >= max_reactor_volume:
-                return f_in_v
-            else:
-                return 0
-
-        def cstr_f_out_calc_tensorflow(max_reactor_volume, f_in_v, volume):
-            return f_in_v * (1 + (99 * volume / max_reactor_volume)) / 100
-            # f_in_v = tf.keras.backend.get_value(f_in_v)
-            # volume = tf.keras.backend.get_value(volume)
-            # max_reactor_volume = tf.keras.backend.get_value(max_reactor_volume)
-            return tf.cond(
-                tf.greater_equal(
-                    tf.cast(volume, tf.float32), tf.cast(max_reactor_volume, tf.float32)
-                ),
-                lambda: tf.cast(f_in_v, tf.float32),
-                lambda: 0.0,
-            )
-            if volume >= max_reactor_volume:
-                return f_in_v
-            else:
-                return 0
-
-        # ----------------------------
-        # NUMERICAL
-        # ----------------------------
-        num_results = run_numerical_methods(
-            initial_state=initial_state,
-            eq_params=eq_params,
-            process_params=process_params,
-            f_out_value_calc=cstr_f_out_calc_numeric,
-        )
-        pinn_results = []
-        start_time = timer()
-        pinn_results, best_pinn_test_index, best_pin_test_error = run_pinn_grid_search(
-            solver_params_list=None,
-            eq_params=eq_params,
-            process_params=process_params,
-            initial_state=initial_state,
-            f_out_value_calc=cstr_f_out_calc_tensorflow,
-            cases_to_try=cases_to_try_vs_simplex(eq_params, process_params),
-        )
-        end_time = timer()
-        print(f"elapsed cstr pinn grid time = {end_time - start_time} secs")
-        # Plot
-        plot_compare_pinns_and_num(
-            title="Comparison of PINN and Numerical Results for CSTR varying ts",
-            pinns=pinn_results,
-            nums=num_results,
-            eq_params=eq_params,
-        )
-
-    if run_fed_batch:
-        process_params = ProcessParams(
-            max_reactor_volume=5,
-            inlet=ConcentrationFlow(
-                volume=0.07,
-                X=eq_params.Xo,
-                P=eq_params.Po,
-                S=eq_params.So,
-            ),
-            t_final=24,  # 10,
-        )
-        # ----------------------------
-        # NUMERICAL
-        # ----------------------------
-        num_results = run_numerical_methods(
-            initial_state=initial_state,
-            eq_params=eq_params,
-            process_params=process_params,
-            f_out_value_calc=lambda max_reactor_volume, f_in_v, volume: 0,
-        )
-        pinn_results = []
-        start_time = timer()
-        pinn_results, best_pinn_test_index, best_pin_test_error = run_pinn_grid_search(
-            solver_params_list=None,
-            eq_params=eq_params,
-            process_params=process_params,
-            initial_state=initial_state,
-            f_out_value_calc=lambda max_reactor_volume, f_in_v, volume: 0,
-            cases_to_try=cases_to_try_ts(eq_params, process_params),
-        )
-        end_time = timer()
-        print(f"elapsed fedbatch reactor pinn grid time = {end_time - start_time} secs")
-        # Plot
-        plot_compare_pinns_and_num(
-            title="Comparison of PINN and Numerical Results for feedbatch reactor",
-            pinns=pinn_results,
-            nums=num_results,
-            eq_params=eq_params,
-        )
-
-    if run_batch:
-        process_params = ProcessParams(
-            max_reactor_volume=5,
-            inlet=ConcentrationFlow(
-                volume=0.0,
-                X=eq_params.Xo,
-                P=eq_params.Po,
-                S=eq_params.So,
-            ),
-            t_final=10.2,
-        )
-        # ----------------------------
-        # NUMERICAL
-        # ----------------------------
-        num_results = run_numerical_methods(
-            initial_state=initial_state,
-            eq_params=eq_params,
-            process_params=process_params,
-            f_out_value_calc=lambda max_reactor_volume, f_in_v, volume: 0,
-        )
-
-        # ----------------------------
-        # PINN
-        # ----------------------------
-        start_time = timer()
-        pinn_results, best_pinn_test_index, best_pin_test_error = run_pinn_grid_search(
-            solver_params_list=None,
-            eq_params=eq_params,
-            process_params=process_params,
-            initial_state=initial_state,
-            f_out_value_calc=lambda max_reactor_volume, f_in_v, volume: 0,
-            cases_to_try=cases_to_try_ts(eq_params, process_params),
-        )
-        end_time = timer()
-        print(f"elapsed batch reactor pinn grid time = {end_time - start_time} secs")
-
-        # Plot
-        plot_compare_pinns_and_num(
-            pinns=pinn_results, num=num_results, eq_params=eq_params
-        )
 
     print("--------------------")
     print("!!!!!!FINISED!!!!!!")
