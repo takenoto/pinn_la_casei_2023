33bcea599d6758d579051e1714aba2a442b50c8a
takenoto <mendesrenan5@gmail.com>
2023-07-23 17:15:39 -0300

    added time (t) to SystemSimulationType

--- domain/params/solver_params.py
+++ domain/params/solver_params.py
@@ -1,3 +1,5 @@
+# python -m domain.params.solver_params
+
 from domain.optimization.non_dim_scaler import NonDimScaler
 
 
@@ -13,12 +15,14 @@ class SystemSimulationType:
     de cada variável no ODE_preparer
     """
     def __init__(self, supported_variables=['X', 'P', 'S', 'V']):
+        self.t = 't' in supported_variables
         self.X = 'X' in supported_variables
         self.P = 'P' in supported_variables
         self.S = 'S' in supported_variables
         self.V = 'V' in supported_variables
 
         # Só pra ficar mais fácil de digitar abaixo
+        t= self.t
         X = self.X
         P = self.P
         S = self.S
@@ -26,12 +30,15 @@ class SystemSimulationType:
 
         # Estabelece o index de cada uma, sempre seguindo um padrão,
         # que independe de "supported_variables"
-        self.X_index = 0 if X else None
-        self.P_index = 1 if P and X else None
-        self.S_index = 2 if P and S else None # o S não pode ser calculado sem o P
-        self.V_index = 3 if X and P and S else 2 if P else 1 if X else 0 if V else None;
+        self.t_index = 0 if t else None
+        self.X_index = None if not X else (self.t_index+1 if t else 0)
+        self.P_index = None if not P else (self.X_index+1 if X else self.t_index+1 if t else 0) #1 if P and X else 1 if P else None
+        self.S_index = None if not S else (self.P_index+1 if P else self.X_index+1 if X else self.t_index+1 if t else 0) #2 if P and S else None # o S não pode ser calculado sem o P
+        self.V_index = None if not V else (self.S_index+1 if S else self.P_index+1 if P else self.X_index+1 if X else self.t_index+1 if t else 0)#3 if X and P and S else 2 if P else 1 if X else 0 if V else None;
 
         self.order = []
+        if(t):
+            self.order.append('t')
         if(X):
             self.order.append('X')
         if(P):
@@ -135,6 +142,22 @@ if __name__ == "__main__":
     assert xv.S_index == None
     assert xv.V_index == 1
     assert xv.order[0] == 'X'
+    assert xv.order[1] == 'V'
+    assert len(xv.order) == 2
+
+    # Sistema com tempo
+    xv = SystemSimulationType(['t', 'V'])
+    assert xv.t == True
+    assert xv.X == False
+    assert xv.P == False
+    assert xv.S == False
+    assert xv.V == True
+    assert xv.t_index == 0
+    assert xv.X_index == None
+    assert xv.P_index == None
+    assert xv.S_index == None
+    assert xv.V_index == 1
+    assert xv.order[0] == 't'
     assert xv.order[1] == 'V'
     assert len(xv.order) == 2
 
