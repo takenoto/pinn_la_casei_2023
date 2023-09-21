b4748cf503490ceea2be7a8f5ae66cc6342afed9
takenoto <mendesrenan5@gmail.com>
2023-09-21 11:20:06 -0300

    change formatter so all variables are math text

--- data/sci_tick_formatter.py
+++ data/sci_tick_formatter.py
@@ -5,10 +5,10 @@ from matplotlib import pyplot as plt
 # ref: https://stackoverflow.com/questions/20127388/scientific-notation-on-each-tick-in-the-default-font-in-pyplot
 def SciFormatter(x, lim):
     if x <= 100 and x >= 0.01:
-        return "{0:.1f}".format(x)
+        return "${0:.1f}$".format(x)
 
     if x == 0:
-        return 0
+        return "$0$"
 
     firstNum = "{0:.1f}".format(
         np.sign(x) * 10 ** (-np.floor(np.log10(abs(x))) + np.log10(abs(x)))
@@ -21,4 +21,4 @@ def SciFormatter(x, lim):
 
     # about math regular:
     # https://stackoverflow.com/questions/27698377/how-do-i-make-sans-serif-superscript-or-subscript-text-in-matplotlib
-    return "$\mathregular" + f"{firstNum}x10^{exponent}" + "$"
+    return "$" + f"{firstNum}x10^{exponent}" + "$"
