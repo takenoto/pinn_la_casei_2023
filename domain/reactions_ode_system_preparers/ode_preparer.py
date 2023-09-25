81d71a8072cc85e4ea8a76078daddcdf6360232b
takenoto <mendesrenan5@gmail.com>
2023-09-25 07:54:28 -0300

    loss v6 XPS implemented defaulting to loss v5

--- domain/reactions_ode_system_preparers/ode_preparer.py
+++ domain/reactions_ode_system_preparers/ode_preparer.py
@@ -230,19 +230,8 @@ class ODEPreparer:
                     loss_derivative = dXdt * V - (V * rX + (f_in * inlet.X - f_out * X))
                     loss_X = 0
                     if loss_version >= 6:
-                        loss_maxmin = tf.where(
-                            tf.less(X, 0),
-                            X,
-                            tf.where(
-                                tf.greater(X, tf.ones_like(X) * Xm),
-                                tf.ones_like(X) * Xm,
-                                tf.zeros_like(X),
-                            ),
-                        )
-                        loss_derivative_abs = tf.abs(loss_derivative)
-                        loss_maxmin_abs = tf.abs(loss_maxmin)
-                        loss_X = loss_derivative_abs + loss_maxmin_abs
-                    elif loss_version == 5:
+                        pass
+                    if loss_version >= 5:
                         loss_maxmin = tf.where(
                             tf.less(X, 0),
                             tf.math.pow(X, 3),
@@ -275,19 +264,8 @@ class ODEPreparer:
                     loss_derivative = dPdt * V - (V * rP + (f_in * inlet.P - f_out * P))
                     loss_P = 0
                     if loss_version >= 6:
-                        loss_maxmin = tf.where(
-                            tf.less(P, 0),
-                            P,
-                            tf.where(
-                                tf.greater(P, Pm),
-                                tf.ones_like(P) * Pm,
-                                tf.zeros_like(P),
-                            ),
-                        )
-                        loss_derivative_abs = tf.abs(loss_derivative)
-                        loss_maxmin_abs = tf.abs(loss_maxmin)
-                        loss_P = loss_derivative_abs + loss_maxmin_abs
-                    elif loss_version == 5:
+                        pass
+                    if loss_version >= 5:
                         loss_maxmin = tf.where(
                             tf.less(P, 0),
                             tf.math.pow(P, 3),
@@ -318,19 +296,8 @@ class ODEPreparer:
                     loss_derivative = dSdt * V - (V * rS + (f_in * inlet.S - f_out * S))
                     loss_S = 0
                     if loss_version >= 6:
-                        loss_maxmin = tf.where(
-                            tf.less(S, 0),
-                            S,
-                            tf.where(
-                                tf.greater(S, tf.ones_like(S) * initial_state.S[0]),
-                                tf.ones_like(S) * initial_state.S[0],
-                                tf.zeros_like(S),
-                            ),
-                        )
-                        loss_derivative_abs = tf.abs(loss_derivative)
-                        loss_maxmin_abs = tf.abs(loss_maxmin)
-                        loss_S = loss_derivative_abs + loss_maxmin_abs
-                    elif loss_version == 5:
+                        pass
+                    if loss_version >= 5:
                         loss_maxmin = tf.where(
                             tf.less(S, 0),
                             tf.math.pow(S, 3),
@@ -367,12 +334,12 @@ class ODEPreparer:
                     if loss_version >= 6:
                         loss_maxmin = tf.where(
                             tf.less(V, 0),
-                            V,
+                            tf.math.pow(V, 3),
                             tf.zeros_like(V),
                         )
                         loss_derivative_abs = tf.abs(loss_derivative)
                         loss_maxmin_abs = tf.abs(loss_maxmin)
-                        loss_V = loss_derivative_abs + loss_maxmin_abs
+                        loss_V = 100*loss_derivative_abs + loss_maxmin_abs
                     elif loss_version == 5:
                         loss_maxmin = tf.where(
                             tf.less(V, 0),
