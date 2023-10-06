0f8beb577ac717413ee14b749b156b7e98ce7981
takenoto <mendesrenan5@gmail.com>
2023-10-06 11:50:06 -0300

    add 2 order derivatives support

--- domain/numeric_solver/numeric_solver_model_results.py
+++ domain/numeric_solver/numeric_solver_model_results.py
@@ -10,10 +10,16 @@ class NumericSolverModelResults:
         t,
         dt,
         non_dim_scaler,
+        # 1 order derivatives
         dX_dt,
         dP_dt,
         dS_dt,
         dV_dt,
+        # 2 order derivatives
+        dX_dt_2,
+        dP_dt_2,
+        dS_dt_2,
+        dV_dt_2,
     ):
         """
         model_name é algo como "euler" ou "runge_kutta"
@@ -32,3 +38,7 @@ class NumericSolverModelResults:
         self.dP_dt = dP_dt
         self.dS_dt = dS_dt
         self.dV_dt = dV_dt
+        self.dX_dt_2 = dX_dt_2
+        self.dP_dt_2 = dP_dt_2
+        self.dS_dt_2 = dS_dt_2
+        self.dV_dt_2 = dV_dt_2
