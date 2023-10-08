192f49bcecdc72af0488fb4eb8a28f7cb4725400
takenoto <mendesrenan5@gmail.com>
2023-10-08 12:24:30 -0300

    create to_dict function

--- domain/params/process_params.py
+++ domain/params/process_params.py
@@ -1,7 +1,16 @@
 from domain.flow.concentration_flow import ConcentrationFlow
 
 class ProcessParams:
-    def __init__(self, max_reactor_volume, inlet: ConcentrationFlow, t_final):
+    def __init__(self, max_reactor_volume, inlet: ConcentrationFlow, t_final, Smax):
         self.max_reactor_volume = max_reactor_volume
         self.inlet = inlet
         self.t_final = t_final
+        self.Smax = Smax
+        
+    def to_dict(self):
+        return {
+            "max_reactor_volume":self.max_reactor_volume,
+            "inlet": self.inlet.to_dict(),
+            "t_final": self.t_final,
+            "Smax": self.Smax,
+        }
