c2945b7ff1f7f44abcaf9ba93576226bf6f9ff1a
takenoto <mendesrenan5@gmail.com>
2023-10-08 11:56:56 -0300

    add lossv7 variations

--- domain/reactions_ode_system_preparers/losses/picker.py
+++ domain/reactions_ode_system_preparers/losses/picker.py
@@ -9,6 +9,7 @@ from domain.reactions_ode_system_preparers.losses.versions import (
     lossV7,
 )
 
+_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 def loss_picker(loss_version, o, args):
     assert o in ["X", "P", "S", "V"], "Key must be X,P,S or V"
@@ -17,8 +18,9 @@ def loss_picker(loss_version, o, args):
     all_args = args
     # Other losses only the first 19
     args = args[:18]
-    if loss_version == 7:
-        return lossV7(o, all_args)
+    # "7A", "7B", ... até "7Z"
+    if loss_version in [f"7{letter}" for letter in _alphabet]: 
+        return lossV7(o, all_args, loss_version)
     if loss_version == 6:
         return lossV6(o, args)
     elif loss_version == 5:
