f29d8fd6c3bcae3de9ecb32a533d025618c4cde2
takenoto <mendesrenan5@gmail.com>
2023-10-09 08:51:07 -0300

    create

---
+++ utils/colors.py
@@ -0,0 +1,25 @@
+xp_colors = ["#7e35b5"]
+"""
+Cores apra dados experimentais
+"""
+pinn_colors = [
+    "olivedrab",  # "#7293A0",
+    "darkorange",  # "#C2E812",
+    "#ED9B40",
+    "#B4869F",
+    "#45B69C",
+    "#FFE66D",
+    "#E78F8E",
+    "#861388",
+    "#045275",
+]
+"""
+Lista de cores que representam diversos pinns
+Ordem: RGBA
+"""
+
+num_colors = [
+    "b",
+    "#F39B6D",
+    "#F0C987",
+]
